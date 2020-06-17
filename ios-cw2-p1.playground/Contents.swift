6393cf3236b51b2e2200198f971c692d02041fa2
moalshaaban <66526800+moalshaaban@users.noreply.github.com>
2020-06-17 23:26:40 +0300

    التمارين ال٣
    اساسيات جداً مهمه و يعطيكم الف عافيه على المجهود

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+
+let family = ["abdualla", "salem", "mohammed"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grabes = [82.4, 90.4, 93.8]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[2])
+print(evens[0], evens[5])
+print(grabes[0], grabes[2])
+print(trueFalse[0], trueFalse[3])
+
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+
+var fruits = [ "mango", "orange", "potato", "peach" ]
+
+print(fruits[0])
+print(fruits[3])
+
+fruits.append("strawbrerry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+
+fruits += ["kiwi", "watermelon", "banana", "pineapple" ]
+ 
+print(fruits)
+
+fruits.removeAll()
+print(fruits)
